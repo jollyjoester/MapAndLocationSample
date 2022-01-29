1902ffd918af809adc436aeba404b3ee730d3632
jollyjoester <jollyjoester@gmail.com>
2022-01-30 01:06:38 +0900

    Render line

--- MapAndLocationSample/ViewController.swift
+++ MapAndLocationSample/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     
     var locationInfoList: [LocationInfo] = []
     var currentLocation: CLLocationCoordinate2D?
+    var userLocationLogs: [CLLocationCoordinate2D] = []
     
     var locationManager = CLLocationManager()
     
@@ -22,18 +23,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         locationManager.delegate = self
+        mapView.delegate = self
         
         locationManager.startUpdatingLocation()
         locationManager.requestWhenInUseAuthorization()
         
+        // 現在地と表示する領域の広さ等を設定
         mapView.showsUserLocation = true
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: span)
+        mapView.region = region
     }
     
     private func addAnnotations(with locationInfoList: [LocationInfo]) {
+        
+        // いったん全部のAnnotationを削除
+        mapView.removeAnnotations(mapView.annotations)
+        
+        // 記録してある位置情報ごとにAnnotationを追加
         for locationInfo in locationInfoList {
             let annotation = MKPointAnnotation()
             annotation.coordinate = locationInfo.coordinate
             
+            // 日時を文字列で表現するためのフォーマットを指定
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
             annotation.title = dateFormatter.string(from: locationInfo.recordedAt)
@@ -42,10 +54,17 @@ class ViewController: UIViewController {
         }
     }
     
-    private func removeAnnotations() {
-        mapView.removeAnnotations(mapView.annotations)
+    private func addRoute(with userLocationLogs: [CLLocationCoordinate2D]) {
+        
+        print(userLocationLogs.count)
+        // いったんオーバーレイを削除
+        mapView.removeOverlays(mapView.overlays)
+        
+        // 位置情報（coordinate）のリストを渡してラインを描画
+        let line = MKPolyline(coordinates: userLocationLogs, count: userLocationLogs.count)
+        mapView.addOverlay(line)
     }
-
+    
     @IBAction func tappedRecord(_ sender: UIButton) {
         
         if let currentLocation = currentLocation {
@@ -57,14 +76,16 @@ class ViewController: UIViewController {
     @IBAction func tappedDisplayAnnotations(_ sender: UIButton) {
         self.addAnnotations(with: locationInfoList)
     }
-    
-    @IBAction func tappedDeleteAnnotations(_ sender: UIButton) {
-        self.removeAnnotations()
+
+    @IBAction func tappedDisplayRoute(_ sender: UIButton) {
+        self.addRoute(with: userLocationLogs)
     }
+    
 }
 
 extension ViewController: CLLocationManagerDelegate {
     
+    // 位置情報を取得する度に呼ばれる
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         let location: CLLocation? = locations.last
@@ -73,6 +94,7 @@ extension ViewController: CLLocationManagerDelegate {
             let coordinate = location.coordinate
 
             currentLocation = coordinate
+            userLocationLogs.append(coordinate)
             
             let longitude = coordinate.longitude
             let latitude = coordinate.latitude
@@ -81,5 +103,22 @@ extension ViewController: CLLocationManagerDelegate {
             
             mapView.setCenter(coordinate, animated: true)
         }
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        // Lineの色や太さを設定。デフォルトでは太さが0なので描画されない
+        if overlay is MKPolyline {
+            let polyLineRenderer = MKPolylineRenderer(overlay: overlay)
+            polyLineRenderer.strokeColor = UIColor.red
+            polyLineRenderer.lineWidth = 2.0
+            
+            // Lineをドットパターンにする場合は下記のような設定をする。
+            //polyLineRenderer.lineDashPattern = [1, 7]
+            return polyLineRenderer
+        }
+        return MKOverlayRenderer()
     }
 }
